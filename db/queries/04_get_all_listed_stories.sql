93c89b9dc579283f00e14490d8391c8f16e064b6
blacitea <angelhoikei@gmail.com>
2020-08-22 22:15:50 +0000

    add query for all listed stories

---
+++ db/queries/04_get_all_listed_stories.sql
@@ -0,0 +1,13 @@
+SELECT
+  stories.id,
+  stories.title,
+  users.name AS author,
+  stories.completed,
+  stories.deleted
+  FROM
+    stories
+    JOIN users ON users.id = stories.owner_id
+  WHERE
+    stories.deleted = FALSE
+  ORDER BY
+    stories.id;
