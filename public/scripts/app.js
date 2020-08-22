914893fc38d1aa3a7bc8764a6d975d6a7831ee97
Em Urdaneta <em.urdaneta94@gmail.com>
2020-08-22 23:13:20 +0000

    create homepage layout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,18 @@
 $(() => {
+
+  /*scrolling effect on nav*/
+  $(".menu-icon").on("click", function() {
+      $("nav ul").toggleClass("showing");
+    });
+  });
+
+  $(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+      $('nav').addClass('black');
+    } else {
+    $('nav').removeClass('black');
+  }
+  /*scroll ends here*/
   $.ajax({
     method: "GET",
     url: "/api/users"
