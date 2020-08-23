c1e6a3646aac6174d744b0d331cf585889511891
blacitea <angelhoikei@gmail.com>
2020-08-23 00:08:01 +0000

    add seeds for favourites and fix typo

---
+++ db/seeds/05_favourites.sql
@@ -0,0 +1,11 @@
+-- Widgets table seeds here (Example)
+INSERT INTO favourites (user_id, story_id) VALUES (2, 1);
+INSERT INTO favourites (user_id, story_id) VALUES (5, 2);
+INSERT INTO favourites (user_id, story_id) VALUES (1, 3);
+INSERT INTO favourites (user_id, story_id) VALUES (2, 4);
+INSERT INTO favourites (user_id, story_id) VALUES (6, 4);
+INSERT INTO favourites (user_id, story_id) VALUES (7, 4);
+INSERT INTO favourites (user_id, story_id) VALUES (2, 5);
+INSERT INTO favourites (user_id, story_id) VALUES (3, 4);
+INSERT INTO favourites (user_id, story_id) VALUES (4, 9);
+INSERT INTO favourites (user_id, story_id) VALUES (2, 9);
