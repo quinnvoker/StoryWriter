a8d9d633b388f0c69a817ef412f745a9901a1e29
blacitea <angelhoikei@gmail.com>
2020-08-23 01:19:59 +0000

    fix query string to remove duplicated rows

--- db/queries/10_get_favourite_stories_by_user_id.sql
+++ db/queries/10_get_favourite_stories_by_user_id.sql
@@ -1,13 +1,15 @@
 SELECT
   stories.id AS story_id,
   stories.title AS title,
-  completed,
-  accepted_at AS last_update
+  MAX(contributions.accepted_at) AS last_update,
+  completed
   FROM
     favourites
     JOIN stories ON stories.id = favourites.story_id
     LEFT JOIN contributions ON stories.id = contributions.story_id
   WHERE
     favourites.user_id = 3
+  GROUP BY
+    stories.id, title, completed
   ORDER BY
     story_id;
