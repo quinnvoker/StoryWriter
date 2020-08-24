280ab8dfa40196de31b58aceeb82ad7f5b9f6dad
blacitea <angelhoikei@gmail.com>
2020-08-24 20:36:43 +0000

    update stories schema to include created_at

--- db/schema/02_stories.sql
+++ db/schema/02_stories.sql
@@ -5,6 +5,7 @@ CREATE TABLE stories (
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   cover_image_url VARCHAR(255) DEFAULT 'https://specials-images.forbesimg.com/imageserve/5d4e3c3bec8b3e00086d4298/960x0.jpg?fit=scale',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   completed BOOLEAN NOT NULL DEFAULT FALSE,
   deleted BOOLEAN NOT NULL DEFAULT FALSE
 )
