ed1768fbd38cdb6e876e27d9b269e35a2b19b8a5
Quinn <quinnvoker@gmail.com>
2020-08-25 01:29:24 +0000

    cleaned up unneccessary views

--- public/scripts/components/index.js
+++ public/scripts/components/index.js
@@ -1,13 +1,5 @@
 $(() => {
 
-  window.loadAllStories();
   views_manager.show('stories');
-
-  deleteStory({story_id: 8});
-
-  getAllStories()
-    .then(json => {
-      console.log(json);
-    });
 
 });
--- public/scripts/views_manager.js
+++ public/scripts/views_manager.js
@@ -4,26 +4,20 @@ $(() => {
 
   window.views_manager = {
     show(item) {
-      $home.detach();
-      $myStories.detach();
       $story.detach();
       $stories.detach();
 
       switch (item) {
-        case 'test_1':
-          $test_1.appendTo($main);
-          break;
-        case 'test_2':
-          $test_2.appendTo($main);
-          break;
         case 'home':
           $home.appendTo($main);
           break;
         case 'stories':
+          loadAllStories();
           $stories.appendTo($main);
           break;
         case 'myStories':
-          $myStories.appendTo($main);
+          loadMyStories();
+          $stories.appendTo($main);
           break;
         case 'story':
           $story.appendTo($main);
