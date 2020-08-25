9d2265d98065a755aad7f71a432fdbc207473e24
Quinn <quinnvoker@gmail.com>
2020-08-25 02:45:00 +0000

    navbar link functionality added

--- public/scripts/components/navbar.js
+++ public/scripts/components/navbar.js
@@ -9,13 +9,29 @@ $(() => {
     </div>
     <div class="menu">
         <ul>
-          <li><a href="#">Home</a></li>
+          <li><a id="home" href="#">Home</a></li>
           <li><a id="my-stories" href="#">My Stories</a></li>
           <li><a id="contributions" href="#">Contributions</a></li>
         </ul>
     </div>
   </nav>
   `);
+
+  $(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+      $navbar.addClass('black');
+    } else {
+      $navbar.removeClass('black');
+    }
+  });
+
+  $navbar.find('#home').click(() => {
+    views_manager.show('home');
+  });
+
+  $navbar.find('#my-stories').click(() => {
+    views_manager.show('myStories');
+  });
 
   $navbar.prependTo($('body'));
 });
