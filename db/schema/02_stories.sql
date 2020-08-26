8a5cfed63a9a4a392a553d90b4dc5d1a49396541
blacitea <angelhoikei@gmail.com>
2020-08-26 01:28:48 +0000

    expend the seeds

--- db/schema/02_stories.sql
+++ db/schema/02_stories.sql
@@ -4,7 +4,7 @@ CREATE TABLE stories (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
-  cover_image_url VARCHAR(255) DEFAULT 'https://specials-images.forbesimg.com/imageserve/5d4e3c3bec8b3e00086d4298/960x0.jpg?fit=scale',
+  cover_image_url TEXT DEFAULT 'https://specials-images.forbesimg.com/imageserve/5d4e3c3bec8b3e00086d4298/960x0.jpg?fit=scale',
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   completed BOOLEAN NOT NULL DEFAULT FALSE,
   deleted BOOLEAN NOT NULL DEFAULT FALSE
--- public/scripts/components/index.js
+++ public/scripts/components/index.js
@@ -1,15 +1,5 @@
 $(() => {
 
-  // // getStory({story_id: 6})
-  // //   .then(result => {
-  // //     console.log(result);
-  // //     $('body').append(createStoryInfo(result));
-  // //   });
-
-  // generateStoryView(6);
-  // $('body').append($story);
-
-
   views_manager.show('home');
 
 });
