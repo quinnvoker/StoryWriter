254f6c882748e95fdc3d6bd8b427d020b98a0cf1
Quinn <quinnvoker@gmail.com>
2020-08-26 20:21:24 +0000

    fixed readmore links on user contributions page

--- public/scripts/components/contributions.js
+++ public/scripts/components/contributions.js
@@ -7,7 +7,7 @@ $(() => {
         <h6 class="card-subtitle mb-2 text-muted"></h6>
         <p class="card-text"></p>
         <i class="fas fa-thumbs-up"></i><span class="like-counter">${contribution.contribution_vote_count} </span>
-        <a id="${contribution.contribution_id}" href="#" class="read-more text-right">Read more <i class="fas fa-chevron-right"></i></a>
+        <a class="read-more text-right" href="#">Read more <i class="fas fa-chevron-right"></i></a>
       </div>
     </div>
     `);
@@ -15,6 +15,11 @@ $(() => {
     $contribution.find('h5').text(contribution.story_title);
     $contribution.find('p.card-text').text(contribution.contribution_content);
     $contribution.find('h6.text-muted').text(moment(contribution.contribution_created_at_time).format('MM/DD/YYYY'));
+
+    $contribution.find('.read-more').on('click',function() {
+      generateContrView(contribution.contribution_id);
+      views_manager.show('contribution');
+    });
 
     $contribution.find('i.fa-thumbs-up').on('click',()=>{
       alert("clicked");
