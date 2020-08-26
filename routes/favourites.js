85ee1ec7c24c8efd19df1c1895e2f38197ccad9b
blacitea <angelhoikei@gmail.com>
2020-08-26 21:03:29 +0000

    add router for favourites

---
+++ routes/favourites.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (queryFunctions) => {
+  router.post("/:id", (req, res) => {
+    const queryParams = [req.session.user_id, req.params.id];
+    queryFunctions.createVote(queryParams)
+      .then(vote => {
+        res.json(vote);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    queryFunctions.getVoteCount({contribution_id: req.params.id})
+      .then(vote => {
+        console.log(vote);
+        res.json(vote);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
