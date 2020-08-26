76c89231733f766a17dffd6f11b1ba950ae84efa
Em Urdaneta <em.urdaneta94@gmail.com>
2020-08-26 01:48:20 +0000

    adding click listener to vote

--- public/scripts/components/contributions.js
+++ public/scripts/components/contributions.js
@@ -6,7 +6,7 @@ $(() => {
         <h5 class="card-title"></h5>
         <h6 class="card-subtitle mb-2 text-muted"></h6>
         <p class="card-text"></p>
-        <i class="fas fa-thumbs-up"></i><span class="like-counter">${contribution.contribution_vote_count}</span>
+        <i class="fas fa-thumbs-up"></i><span class="like-counter">${contribution.contribution_vote_count} votes</span>
         <a id="${contribution.contribution_id}" href="#" class="read-more text-right">Read more <i class="fas fa-chevron-right"></i></a>
       </div>
     </div>
@@ -15,6 +15,14 @@ $(() => {
     $contribution.find('h5').text(contribution.contribution_id);
     $contribution.find('p.card-text').text(contribution.contribution_content);
     $contribution.find('h6.text-muted').text(moment(contribution.contribution_created_at_time).format('MM/DD/YYYY'));
+
+    $contribution.find('i.fa-thumbs-up').on('click',()=>{
+      alert("clicked");
+      const $votes = $contribution.find('.like-counter').text();
+      const $count = $votes.split(' ');
+      $contribution.find('i.fa-thumbs-up').addClass('orange');
+      $contribution.find('.like-counter').text(parseInt($count)+1);
+    })
 
     return $contribution;
   };
