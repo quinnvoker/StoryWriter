eafe9bf19b5c78847fba8c00b4c79e5d635fc114
Quinn <quinnvoker@gmail.com>
2020-08-26 02:25:25 +0000

    contribution composition form now also has error handling

--- public/scripts/components/compose_contribution.js
+++ public/scripts/components/compose_contribution.js
@@ -4,11 +4,16 @@ $(() => {
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <button type="button" class="close exit" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
+          <div class="error-panel">
+            <i class="fas fa-exclamation-triangle"></i>
+            <span class="error"></span>
+            <i class="fas fa-exclamation-triangle"></i>
+          </div>
           <form id="new-story">
             <textarea name="text" id="contribution-content" placeholder="Continue the adventure..."></textarea>
             <div>
@@ -16,12 +21,7 @@ $(() => {
           </form>
         </div>
         <div class="modal-footer">
-          <div class="error-panel">
-            <i class="fas fa-exclamation-triangle"></i>
-            <span class="error"></span>
-            <i class="fas fa-exclamation-triangle"></i>
-          </div>
-          <button type="button" class="secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="secondary exit" data-dismiss="modal">Close</button>
           <button id="submit-contribution" type="button" class="orange">Submit</button>
         </div>
       </div>
@@ -29,21 +29,20 @@ $(() => {
   </div>
   `);
 
+  const $errorPanel = $composeContribution.find('.error-panel');
+  $errorPanel.hide();
+
   $composeContribution.find('#submit-contribution')
     .click((event) => {
       event.preventDefault();
       const content = $composeContribution.find('#contribution-content').val();
 
-      const $errorPanel = $composeStory.find('.error-panel');
-
-      if ($errorPanel.is(':visible')) {
-        $errorPanel.slideUp();
-      }
-
-      if (title.length < 1 || content.length < 1) {
-        $errorPanel.find('.error').text('Cannot submit story without title or content!');
+      if (content.length < 1) {
+        $errorPanel.find('.error').text('Cannot submit contribution with no content!');
         $errorPanel.slideDown();
         return;
+      } else if ($errorPanel.is(':visible')) {
+        $errorPanel.slideUp();
       }
 
       const contributionData = { content, story_id: window.targetStoryId };
@@ -52,6 +51,13 @@ $(() => {
           $composeContribution.modal('hide');
           console.log(result);
         });
+    });
+
+  $composeContribution.find('.exit')
+    .click(() => {
+      if ($errorPanel.is(':visible')) {
+        $errorPanel.slideUp();
+      }
     });
 
   window.$composeContribution = $composeContribution;
