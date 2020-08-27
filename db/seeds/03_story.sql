3e05b2b23e0656a07eca936f9eb6d97e42ef8886
Em Urdaneta <em.urdaneta94@gmail.com>
2020-08-27 23:33:26 +0000

    added hover effect for all buttons

---
+++ db/seeds/03_story.sql
@@ -0,0 +1,17 @@
+INSERT INTO stories (owner_id, title, cover_image_url, completed)
+  VALUES
+    (19, 'The Scream', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQukufE6H94xVUrupLdc4fZuWVXAoUle8e3wn3MssvGMd8eFgaE_w-s1w1fFwGlbJ8XYH1Gz8M9pbr9s2dXtnE0RVTCBDYCD70whQ&usqp=CAU&ec=45695924');
+
+INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at) VALUES (3, 19, 'Ash and Pikachu were walking down the path that leads to Pallet Town, on their way home from the Kalos Region, after saying farewell to Clement, Bonnie, and Serena at the airport.', '2020-07-19 00:45:02.85335', '2020-07-19 00:46:02.85335');
+
+
+
+INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at) VALUES (3, 16, 'Ash was proud of himself and his Pokémon since the Kalos Leauge, although Ash lost to Alain the battle had only made him stronger.', '2020-07-20 00:45:02.85335', '2020-07-20 00:46:02.85335');
+
+
+INSERT INTO contributions (story_id, user_id, content, created_at) VALUES (3, 18, 'Goodra was back in the Wetlands with its friends. ');
+
+
+INSERT INTO contributions (story_id, user_id, content, created_at) VALUES (3, 14, 'Greninja was helping Zygarde with the root problem in Kalos.');
+
+
