27a9c545a47dd38393810f4f0d335d7b6448a239
Quinn <quinnvoker@gmail.com>
2020-08-27 22:45:51 +0000

    added story entries for 6 & 7

---
+++ db/seeds/06_story.sql
@@ -0,0 +1,3 @@
+INSERT INTO stories (owner_id, title, cover_image_url)
+  VALUES
+    (2, 'The Gatekeepers', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQpAl2T7tO_8TlJ58UNoEM_yCh85FpYrZsQpSkwDA4XRaIJipXGWdA-ceBdh-g1xcWoCa_tV6n8DOUT1-gLi5YHwsluzha263ziwg&usqp=CAU&ec=45695924');
---
+++ db/seeds/07_story.sql
@@ -0,0 +1,3 @@
+INSERT INTO stories (owner_id, title, cover_image_url)
+  VALUES
+    (14, 'The First Official Exploration', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSfhP8e5sbV6vwCfIn83Aa0JxZhAhj815p_AAbAjhKysiPV2GQBM4twHJDdhMHa96w2_YEKZa8-07lMyBxTQgZRhpbL4DOKcxKTwg&usqp=CAU&ec=45695924');
