5b1ae9450692c1f2ce06b99bb89851e941383829
Quinn <quinnvoker@gmail.com>
2020-08-27 23:08:03 +0000

    added contributions for 6

--- db/seeds/06_story.sql
+++ db/seeds/06_story.sql
@@ -1,3 +1,23 @@
 INSERT INTO stories (owner_id, title, cover_image_url)
   VALUES
-    (2, 'The Gatekeepers', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQpAl2T7tO_8TlJ58UNoEM_yCh85FpYrZsQpSkwDA4XRaIJipXGWdA-ceBdh-g1xcWoCa_tV6n8DOUT1-gLi5YHwsluzha263ziwg&usqp=CAU&ec=45695924');
+    (2, 'The New Adventure', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQpAl2T7tO_8TlJ58UNoEM_yCh85FpYrZsQpSkwDA4XRaIJipXGWdA-ceBdh-g1xcWoCa_tV6n8DOUT1-gLi5YHwsluzha263ziwg&usqp=CAU&ec=45695924');
+
+INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at)
+  VALUES
+    (5, 2, '"Welcome to Castelia City!" That was the first thing you saw when you stepped off of the ferry.  You had just moved from the sunny paradise of Alola to the Unova region.  You were sad to leave behind the Professor and your friends, especially Ilima.  You and he had graduated from the school together after all, but instead of you becoming a captain like he chose, you simply wanted your first pokemon.', '2020-07-19 00:45:02.85335', '2020-07-19 00:46:02.85335');
+
+INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at)
+  VALUES
+    (5, 16, 'Thats right you completed the school without an actual pokemon as your companion, but Professor Kukui gave you your first pokemon when he saw your potential. Well, he gave you an egg that you cared for, and eventually it hatched into your very own Alolan Vulpix.  She was special to you.  I mean, she was your first and only pokemon at the time, but no other pokemon would replace her...', '2020-07-20 00:45:02.85335', '2020-07-20 00:46:02.85335');
+
+INSERT INTO contributions (story_id, user_id, content, created_at)
+  VALUES
+    (5, 12, 'Or could they? You only thought she was unreplacable until someone offered you a trade for a cool Scyther!', '2020-07-21 00:45:02.85335');
+
+INSERT INTO contributions (story_id, user_id, content, created_at)
+  VALUES
+    (5, 5, 'You planned to keep this Vulpix as your best friend for your adventures, but she didnt seem to like you much and bit you at every opportunity.', '2020-07-21 00:45:02.85335');
+
+INSERT INTO contributions (story_id, user_id, content, created_at)
+  VALUES
+    (5, 7, 'This Vulpix was your new BFF! You trained really hard and eventually defeated the Pokemon League with your Vulpix as the star of your team! The end.', '2020-07-23 00:45:02.85335');
