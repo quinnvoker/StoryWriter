2151e2d31956c0f4d19e60f2c4f1ae4b00eb5b0b
Quinn <quinnvoker@gmail.com>
2020-08-28 00:00:29 +0000

    re-ordered stories so a completed story appears in middle of list instead of end

--- db/seeds/05_story.sql
+++ db/seeds/05_story.sql
@@ -1,15 +1,20 @@
-INSERT INTO stories (owner_id, title, cover_image_url)
+INSERT INTO stories (owner_id, title, cover_image_url, completed)
   VALUES
-    (19, 'The Scream', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQukufE6H94xVUrupLdc4fZuWVXAoUle8e3wn3MssvGMd8eFgaE_w-s1w1fFwGlbJ8XYH1Gz8M9pbr9s2dXtnE0RVTCBDYCD70whQ&usqp=CAU&ec=45695924');
+    (14, 'The First Official Exploration', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSfhP8e5sbV6vwCfIn83Aa0JxZhAhj815p_AAbAjhKysiPV2GQBM4twHJDdhMHa96w2_YEKZa8-07lMyBxTQgZRhpbL4DOKcxKTwg&usqp=CAU&ec=45695924', TRUE);
 
-INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at) VALUES (4, 19, 'Ash and Pikachu were walking down the path that leads to Pallet Town, on their way home from the Kalos Region, after saying farewell to Clement, Bonnie, and Serena at the airport.', '2020-07-19 00:45:02.85335', '2020-07-19 00:46:02.85335');
+INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at)
+  VALUES
+    (4, 14, 'My alarm goes off for only the third time in my new room. Although Ive been awake for hours. Today is supposed to be the day. All of the important boxes are now empty, and the rest are shoved in the back of the closet. As I slap the head of the Bellsprout shaped alarm clock my Dad bought the day we moved in, I look out the window of my newly built home and I see the sun rising over the horizon of beautiful trees and scenery right next to a place called Sprout Tower. That place gives me a bad vibe, but still, the view is pretty.', '2020-07-19 00:45:02.85335', '2020-07-19 00:46:02.85335');
 
+INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at)
+  VALUES
+    (4, 1, 'I run downstairs to find Dad finished cooking, setting breakfast on the table for me. He loves cooking, and this is the first real meal hes had the chance to make in the house. Weve been surviving off of fast food since we left Pewter City, so Im glad to taste his fresh cooking again.
+Once I sit down, I ask him when were going to leave for New Bark Town to meet Professor Elm. His eyes widen, giving me the impression he forgot that he promised me wed do it today.', '2020-07-20 00:45:02.85335', '2020-07-20 00:46:02.85335');
 
+INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at)
+  VALUES
+    (4, 12, '"You forgot???? How dare you!" I yelled as I put on my jacket, and I ran out of the house. I was going to start my adventure on my own terms! I ran off and became a Pokemon champion! The end!', '2020-07-21 00:45:02.85335', '2020-07-21 00:50:02.85335');
 
-INSERT INTO contributions (story_id, user_id, content, created_at, accepted_at) VALUES (4, 16, 'Ash was proud of himself and his Pokémon since the Kalos Leauge, although Ash lost to Alain the battle had only made him stronger.', '2020-07-20 00:45:02.85335', '2020-07-20 00:46:02.85335');
-
-
-INSERT INTO contributions (story_id, user_id, content, created_at) VALUES (4, 18, 'Goodra was back in the Wetlands with its friends.', '2020-07-21 00:45:02.85335');
-
-
-INSERT INTO contributions (story_id, user_id, content, created_at) VALUES (4, 14, 'Greninja was helping Zygarde with the root problem in Kalos.', '2020-07-22 00:45:02.85335');
+INSERT INTO contributions (story_id, user_id, content, created_at)
+  VALUES
+    (4, 13, 'I look up at him, and ask "You remembered to set up the appointment, right?" He looked at me, suprised, and then smirked as he got up from the table. "Of course I remembered! I even got you your first Pokemon already! Lets go start your adventure!"', '2020-07-21 00:45:02.85335');
